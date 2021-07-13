5f3ced5eab14b7761f83a8457bbfc85ef11df719
kealanheena <kealanheena@gmail.com>
2021-07-13 10:11:26 +0100

    Test: Check for each time block starting and ending at 00:00

--- tests/BolusCalculator.test.ts
+++ tests/BolusCalculator.test.ts
@@ -24,4 +24,12 @@ describe(`#${ClassName}`, () => {
   it(`should have a time blocks property`, () => {
     expect(TestBolusCalculator.timeBlocks).toBeInstanceOf(Object)
   })
+
+  it(`should have 5 time blocks starting at '00:00' and ending at '00:00'`, () => {
+    expect(TestBolusCalculator.timeBlocks['00:00-05:00']).toBeInstanceOf(Object)
+    expect(TestBolusCalculator.timeBlocks['05:00-11:30']).toBeInstanceOf(Object)
+    expect(TestBolusCalculator.timeBlocks['11:30-16:00']).toBeInstanceOf(Object)
+    expect(TestBolusCalculator.timeBlocks['16:00-20:00']).toBeInstanceOf(Object)
+    expect(TestBolusCalculator.timeBlocks['20:00-00:00']).toBeInstanceOf(Object)
+  })
 })

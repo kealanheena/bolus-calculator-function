5d1576303d579d1625367a746206ee1a90d7a399
kealanheena <kealanheena@gmail.com>
2021-07-13 23:23:31 +0100

    Test: add tests for different values for insulin insulinSensitivity

--- tests/BolusCalculator.test.ts
+++ tests/BolusCalculator.test.ts
@@ -47,26 +47,44 @@ describe(`#${ClassName}`, () => {
   })
 
   describe(`#getBolusCorrection`, () => {
+    let insulinSensitivity: number
+
+    beforeEach(() => {
+      insulinSensitivity = TestBolusCalculator.timeBlocks['00:00-05:00'].insulinSensitivity
+    })
+
     it(`should be a function`, () => {
       expect(TestBolusCalculator.getBolusCorrection).toBeInstanceOf(Function)
     })
 
-    it(`should return 1 when 11.0 is passed and insulin sensitivity is equal to 3.0`, () => {
-      expect(TestBolusCalculator.getBolusCorrection(11.0)).toBe(1)
+    describe(`When the insulin sensitivity is 3.0`, () => {
+      it(`should return 1 when 11.0 is passed and insulin sensitivity is equal to 3.0`, () => {
+        expect(TestBolusCalculator.getBolusCorrection(11.0)).toBe(1)
+      })
+  
+      it(`should return 2 when 14.0 is passed and insulin sensitivity is equal to 3.0`, () => {
+        expect(TestBolusCalculator.getBolusCorrection(14.0)).toBe(2)
+      })
+  
+      it(`should round up to 3 when 15.7 is passed and insulin sensitivity is equal to 3.0`, () => {
+        expect(TestBolusCalculator.getBolusCorrection(15.7)).toBe(3)
+      })
     })
 
-    it(`should return 2 when 14.0 is passed and insulin sensitivity is equal to 3.0`, () => {
-      expect(TestBolusCalculator.getBolusCorrection(14.0)).toBe(2)
-    })
+    describe(`When the insulin sensitivity is 4.0`, () => {
+      it(`should return 1 when 12.0 is passed and insulin sensitivity is equal to 4.0`, () => {
+        insulinSensitivity = 4.0
+  
+        expect(TestBolusCalculator.getBolusCorrection(12.0)).toBe(1)
+      })
 
-    it(`should round up to 3 when 15.7 is passed and insulin sensitivity is equal to 3.0`, () => {
-      expect(TestBolusCalculator.getBolusCorrection(15.7)).toBe(3)
-    })
-
-    it(`should round up to 3 when 12.0 is passed and insulin sensitivity is equal to 4.0`, () => {
-      TestBolusCalculator.timeBlocks["00:00-05:00"].insulinSensitivity = 4.0
-
-      expect(TestBolusCalculator.getBolusCorrection(12.0)).toBe(1)
+      it(`should round up to 2 when 14.7 is passed and insulin sensitivity is equal to 3.0`, () => {
+        expect(TestBolusCalculator.getBolusCorrection(14.7)).toBe(2)
+      })
+  
+      it(`should round up to 3 when 15.7 is passed and insulin sensitivity is equal to 3.0`, () => {
+        expect(TestBolusCalculator.getBolusCorrection(19.7)).toBe(4)
+      })
     })
   })
 })

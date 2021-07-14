f7c7ae96eef4c13940024518662c0cec31d6946d
kealanheena <kealanheena@gmail.com>
2021-07-14 15:58:19 +0100

    Refactor: extract setting times from the getCurrentTimeBlock function and create their own private functions

--- src/BolusCalculator.ts
+++ src/BolusCalculator.ts
@@ -9,31 +9,7 @@ export class BolusCalculator {
   }
 
   getBolusCorrection(glucoseReading: number) :number {
-    const currentTime: Date = new Date(),
-          startTime: Date = new Date(currentTime.getTime()),
-          endTime: Date = new Date(currentTime.getTime())
-    
-    let currentTimeBlock: string = '00:00-05:00'
-
-    for (const timeBlock in this.timeBlocks) {
-      const arrayOfTimes: Array<String> = timeBlock.split('-'),
-            startHoursMinsArray: Array<String> = arrayOfTimes[0].split(":"),
-            endHoursMinsArray: Array<String> = arrayOfTimes[1].split(":")
-      
-      startTime.setHours(Number(startHoursMinsArray[0]));
-      startTime.setMinutes(Number(startHoursMinsArray[1]));
-      startTime.setSeconds(0o0);
-
-      endTime.setHours(Number(endHoursMinsArray[0]));
-      endTime.setMinutes(Number(endHoursMinsArray[1]));
-      endTime.setSeconds(0o0);
-
-      console.log(startTime < currentTime, '&', endTime > currentTime)
-      if (startTime < currentTime && endTime > currentTime) {
-        currentTimeBlock = timeBlock
-        break
-      }
-    }
+    const currentTimeBlock: string = this.getCurrentTimeBlock()
 
     const activeTimeBlock: CalculationInfo = this.timeBlocks[currentTimeBlock]
 
@@ -42,5 +18,43 @@ export class BolusCalculator {
     const roundedCorrection: number = Math.round(correctableGlucose/insulinSensitivity)
 
     return roundedCorrection
+  }
+
+  private getCurrentTimeBlock() :string {
+    const currentTime: Date = new Date(),
+          startTime: Date = new Date(currentTime.getTime()),
+          endTime: Date = new Date(currentTime.getTime())
+    
+    let currentTimeBlock: string = '00:00-05:00'
+
+    for (const timeBlock in this.timeBlocks) {
+      const arrayOfTimes: Array<string> = timeBlock.split('-'),
+      
+      this.setStartTime(startTime, arrayOfTimes)
+      this.setEndTime(endTime, arrayOfTimes)
+
+      if (startTime <= currentTime && endTime > currentTime) {
+        currentTimeBlock = timeBlock
+        break
+      }
+    }
+
+    return currentTimeBlock
+  }
+
+  private setStartTime(startTime: Date, arrayOfTimes: Array<string>) {
+    const startHoursMinsArray: Array<string> = arrayOfTimes[0].split(":")
+
+    startTime.setHours(Number(startHoursMinsArray[0]));
+    startTime.setMinutes(Number(startHoursMinsArray[1]));
+    startTime.setSeconds(0o0);
+  }
+
+  private setEndTime(endTime: Date, arrayOfTimes: Array<string>) {
+    const endHoursMinsArray: Array<string> = arrayOfTimes[1].split(":")
+
+    endTime.setHours(Number(endHoursMinsArray[0]));
+    endTime.setMinutes(Number(endHoursMinsArray[1]));
+    endTime.setSeconds(0o0);
   }
 }
